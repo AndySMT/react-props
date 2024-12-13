cfd3ccc75c86d21202d90b65439326bac347fa34
Andy Simota <andyiulian.simota@gmail.com>
2024-12-13 15:35:05 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <ul>
+              <li>Poi vedo che scrivere</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/data/LeagueDB.js
@@ -0,0 +1,104 @@
+const champions = [
+  {
+    id: 1,
+    title: "Ahri",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ahri_86.jpg",
+    content:
+      "Ahri è una Vastaya con una connessione innata alla magia, usa la sua grazia e astuzia per incantare i suoi avversari e sconfiggerli.",
+    tags: ["Luden's Tempest", "Rabadon's Deathcap", "Zhonya's Hourglass"],
+    published: true,
+  },
+  {
+    id: 2,
+    title: "Yasuo",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_86.jpg",
+    content:
+      "Yasuo è un abile spadaccino Ioniano in grado di controllare il vento. La sua abilità e determinazione lo rendono un guerriero letale.",
+    tags: ["Infinity Edge", "Phantom Dancer", "Bloodthirster"],
+    published: true,
+  },
+  {
+    id: 3,
+    title: "Jinx",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Jinx_86.jpg",
+    content:
+      "Jinx è una criminale maniacale con una predilezione per il caos. Armata di esplosivi e armi personalizzate, porta il disordine ovunque vada.",
+    tags: ["Kraken Slayer", "Runaan's Hurricane", "Lord Dominik's Regards"],
+    published: true,
+  },
+  {
+    id: 4,
+    title: "Zed",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Zed_86.jpg",
+    content:
+      "Zed è il maestro dell'ombra e un assassino letale. Usa le ombre per confondere e distruggere i suoi nemici con precisione micidiale.",
+    tags: ["Duskblade of Draktharr", "Youmuu's Ghostblade", "Edge of Night"],
+    published: true,
+  },
+  {
+    id: 5,
+    title: "Lux",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Lux_86.jpg",
+    content:
+      "Lux è una maga di Demacia che incarna la luce stessa. Usa i suoi poteri per proteggere e ispirare gli altri.",
+    tags: ["Luden's Tempest", "Rabadon's Deathcap", "Ardent Censer"],
+    published: true,
+  },
+  {
+    id: 6,
+    title: "Darius",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Darius_86.jpg",
+    content:
+      "Darius è il leader della Trifarian Legion e un guerriero formidabile che schiaccia i suoi nemici con forza bruta.",
+    tags: ["Stridebreaker", "Dead Man's Plate", "Sterak's Gage"],
+    published: false,
+  },
+  {
+    id: 7,
+    title: "Miss Fortune",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/MissFortune_86.jpg",
+    content:
+      "Miss Fortune è una cacciatrice di taglie di Bilgewater, letale e affascinante. Usa i suoi colpi di pistola per annientare i nemici.",
+    tags: ["Kraken Slayer", "Infinity Edge", "The Collector"],
+    published: false,
+  },
+  {
+    id: 8,
+    title: "Ekko",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Ekko_86.jpg",
+    content:
+      "Ekko è un prodigio di Zaun in grado di manipolare il tempo. Usa la sua intelligenza per battere i nemici in modo creativo.",
+    tags: ["Hextech Rocketbelt", "Lich Bane", "Zhonya's Hourglass"],
+    published: false,
+  },
+  {
+    id: 9,
+    title: "Kai'Sa",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Kaisa_86.jpg",
+    content:
+      "Kai'Sa è una cacciatrice implacabile, sopravvissuta alle profondità del Vuoto grazie alla simbiosi con una creatura che la protegge.",
+    tags: ["Kraken Slayer", "Guinsoo's Rageblade", "Runaan's Hurricane"],
+    published: false,
+  },
+  {
+    id: 10,
+    title: "Thresh",
+    image:
+      "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Thresh_86.jpg",
+    content:
+      "Thresh è un custode spettrale di anime, un essere spietato e crudele che si diverte a tormentare i vivi e i morti.",
+    tags: ["Locket of the Iron Solari", "Knight's Vow", "Zeke's Convergence"],
+    published: false,
+  },
+];
+
+export default champions;
